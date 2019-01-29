ba423693327d1daa5db82ab04b331981e4938e58
Luiz Pereira <luizfper@gmail.com>
2019-01-29 19:29:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,10 @@ def prime?(number)
   prime=true
   n = number-1
   while !prime && n!=1
-    prime=false if number%n==0
+    if number%n==0
+      prime=false
+      break
+    end
     n-=1
   end
   return prime
