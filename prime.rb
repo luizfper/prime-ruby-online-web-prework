e062795ca0ce2f22e22ef364f9c2fc28fdf4922a
Luiz Pereira <luizfper@gmail.com>
2019-01-29 19:56:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,9 +4,9 @@ def prime?(number)
     return false
   end
   n = number
-  while prime && n!=0
+  while prime && n!=1
     n-=1
-    prime=false if number%n==0 || n==0
+    prime=false if number%n==0
   end
   return prime
 end
