33f7e3e19f6c73df33e6f2f223cf7bbfea84959e
Luiz Pereira <luizfper@gmail.com>
2019-01-29 19:59:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+  prime=true
+  if number<=1
+    return false
+  end
+  n = number-1
+  while prime && n!=1
+    prime=false if number%n==0
+    n-=1
+  end
+  return prime
+end
